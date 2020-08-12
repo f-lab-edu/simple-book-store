c5fd1c3e8446f6f7b185b3f667af783a1d463da8
youngsuk-dev <63249033+youngsuk-dev@users.noreply.github.com>
2020-08-12 11:07:38 +0900

    path /users 중복 제거

--- src/main/java/com/youngsuk/bookstore/controller/UserController.java
+++ src/main/java/com/youngsuk/bookstore/controller/UserController.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpSession;
  * @RestController 어노테이션을 추가하면 해당 클래스에 있는 핸들러들은 모두 json/XML을 리턴하게 된다.
  */
 @RestController
+@RequestMapping(path = "/users")
 public class UserController {
 
     @Autowired
@@ -29,14 +30,14 @@ public class UserController {
      * 이전에는 @Requestmapping의 속성에 RequestMethod.Post라는 값을 넣어줘야 post 방식으로 값을 받을 수 있었다.
      * 하지만 코드를 짧게 줄이기 위해서 @PostMapping 이라는 어노테이션에 주소값만 추가해주면 post 방식으로 값을 받을 수 있다.
      */
-    @PostMapping(path = "/users")
+    @PostMapping
     public ResponseEntity userAdd(User user) {
         user = userInformationService.makeUserPasswordEncrypt(user);
         user = LoginResponseUtils.makeUserAddResponseInformation(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
-    @PostMapping(path = "/users/login")
+    @PostMapping(path = "/login")
     public ResponseEntity userCheckPasswordGiveSession(User user, HttpServletRequest request) {
         boolean isloginSuccess;
         String LoginMessage;
